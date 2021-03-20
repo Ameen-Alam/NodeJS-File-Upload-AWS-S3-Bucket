d41b2a521b9ca8ddb9af7d6af1d5f41f0b4f5cfc
ameen-alam <ameenalam202@gmail.com>
2021-03-21 02:24:57 +0500

    NodeJS-File-Upload-AWS-S3-Bucket

---
+++ index.js
@@ -0,0 +1,59 @@
+require('dotenv/config')
+const express = require('express')
+const multer = require('multer')
+const AWS = require('aws-sdk')
+const uuid = require('uuid').v4;
+const app = express()
+const port = 3000
+
+const s3 = new AWS.S3({
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+})
+
+const storage = multer.memoryStorage({
+    destination: function(req, file, callback) {
+        callback(null, '')
+    }
+})
+
+const upload = multer({storage}).single('image')
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.post('/upload',upload,(req, res) => {
+
+    let myFile = req.file.originalname.split(".")
+    const fileType = myFile[myFile.length - 1]
+
+    const params = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: `${uuid()}.${fileType}`,
+        Body: req.file.buffer
+    }
+
+    s3.upload(params, (error, data) => {
+        if(error){
+            res.status(500).send(error)
+        }
+
+        res.status(200).send(data)
+    })
+
+    var presignedGETURL = s3.getSignedUrl('getObject', {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: `${uuid()}.${fileType}`,
+        Expires: 1000*5 //time to expire in seconds
+    });
+    console.log(presignedGETURL, " presignedGETURL");
+})
+
+app.listen(port, () => {
+    console.log(`Server is up at ${port}`)
+})
